b30d12b0a9932c1420113c16bf771e2cb7bfcf99
Bekhzod Mukhammadaliev <kingbehzod99@gmail.com>
2024-01-13 21:26:33 +0900

    new repo added

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
 import streamlit as st
+import base64
 from fastai.vision.all import *
 
 # Load the trained model
 path = Path('')
 learn_inf = load_learner(path/'pneumonia_classifier.pkl')
 
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("pict.jpg")
+
+# Add custom CSS to set the background image
+st.markdown(
+    f"""
+    <style>
+    [data-testid="stAppViewContainer"]{{
+    background-image: url('data:image/png;base64,{img}');
+    background-size: cover;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Streamlit app
 st.title("Pneumonia Image Classifier")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-# Display the uploaded image
-image = Image.open(uploaded_file)
-st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-if st.button("Predict"):
-    if uploaded_file is not None:
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
 
+    if st.button("Predict"):
         # Perform prediction
         pred_class, pred_idx, probabilities = learn_inf.predict(image)
 
@@ -24,13 +46,8 @@ if st.button("Predict"):
         if pred_class == "PNEUMONIA":
             result_text += f"<font color='red' style='font-size:30px'>{pred_class}</font>"
         else:
-            result_text += f"<font color='green' style='font-size:3opx'>{pred_class}</font>"
+            result_text += f"<font color='green' style='font-size:30px'>{pred_class}</font>"
 
         # Display Result and Probability in the same line with increased font size
         st.markdown(result_text, unsafe_allow_html=True)
         st.markdown(f"<font style='font-size:30px'>Probability: {probabilities[pred_idx]*100:.2f}%</font>", unsafe_allow_html=True)
-
-
-
-
-
